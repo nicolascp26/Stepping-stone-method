14aef69385690481e9b03c51e00f78f019defcda
Nicolas Carreño <50857034+nicolascp26@users.noreply.github.com>
2022-02-07 14:49:05 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+## Copyright
+
+This program was code by Nicolas Carreño for Optimization class.
+GNU General Public License
+
+## Structure
+
+- `src`: the folder to maintain sources
+
+## Instrucciones
+Nota: Los datos deben ir separados por espacios y siguiendo la siguientes normas:
+
+1. La primera fila es numero de filas (Sources o Centros de produccion) y numero de columnas (Destinations o Centros de venta)
+2. La segunda fila es los valores de las ofertas.
+3. La tercera fila es los valores de las demandas.
+4. Las siguientes filas se colocan los costos, y van de acuerdo a la dimension de la matriz 2*2, 3*3, etc.
---
+++ src/TransportationProblem2.java
@@ -0,0 +1,231 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toCollection;
+
+public class TransportationProblem2 {
+
+    private static int[] demand;
+    private static int[] supply;
+    private static double[][] costs;
+    private static Shipment[][] matrix;
+
+    private static class Shipment {
+        final double costPerUnit;
+        final int r, c;
+        double quantity;
+
+        public Shipment(double q, double cpu, int r, int c) {
+            quantity = q;
+            costPerUnit = cpu;
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    static void init(String filename) throws FileNotFoundException {
+
+        // Lee el archivo que contiene los datos
+        try (Scanner sc = new Scanner(new File(filename))) {
+            int numSources = sc.nextInt();
+            int numDestinations = sc.nextInt();
+
+            List<Integer> src = new ArrayList<>();
+            List<Integer> dst = new ArrayList<>();
+
+            for (int i = 0; i < numSources; i++)
+                src.add(sc.nextInt());
+
+            for (int i = 0; i < numDestinations; i++)
+                dst.add(sc.nextInt());
+
+            // Arregla desbalance
+            int totalSrc = src.stream().mapToInt(i -> i).sum();
+            int totalDst = dst.stream().mapToInt(i -> i).sum();
+            if (totalSrc > totalDst)
+                dst.add(totalSrc - totalDst);
+            else if (totalDst > totalSrc)
+                src.add(totalDst - totalSrc);
+
+            supply = src.stream().mapToInt(i -> i).toArray();
+            demand = dst.stream().mapToInt(i -> i).toArray();
+            // Se crea la matriz costos y una matriz de objetos Cargamento
+            costs = new double[supply.length][demand.length];
+            matrix = new Shipment[supply.length][demand.length];
+            // se llena la matriz de costos
+            for (int i = 0; i < numSources; i++)
+                for (int j = 0; j < numDestinations; j++)
+                    costs[i][j] = sc.nextDouble();
+        }
+    }
+
+    static void northWestCornerRule() {
+
+        for (int r = 0, northwest = 0; r < supply.length; r++)
+            for (int c = northwest; c < demand.length; c++) {
+
+                int quantity = Math.min(supply[r], demand[c]);
+                if (quantity > 0) {
+                    matrix[r][c] = new Shipment(quantity, costs[r][c], r, c);
+
+                    supply[r] -= quantity;
+                    demand[c] -= quantity;
+
+                    if (supply[r] == 0) {
+                        northwest = c;
+                        break;
+                    }
+                }
+            }
+    }
+
+    static void steppingStone() {
+        double maxReduction = 0;
+        Shipment[] move = null;
+        Shipment leaving = null;
+
+        fixDegenerateCase();
+
+        for (int r = 0; r < supply.length; r++) {
+            for (int c = 0; c < demand.length; c++) {
+
+                if (matrix[r][c] != null)
+                    continue;
+
+                Shipment trial = new Shipment(0, costs[r][c], r, c);
+                Shipment[] path = getClosedPath(trial);
+
+                double reduction = 0;
+                double lowestQuantity = Integer.MAX_VALUE;
+                Shipment leavingCandidate = null;
+
+                boolean plus = true;
+                for (Shipment s : path) {
+                    if (plus) {
+                        reduction += s.costPerUnit;
+                    } else {
+                        reduction -= s.costPerUnit;
+                        if (s.quantity < lowestQuantity) {
+                            leavingCandidate = s;
+                            lowestQuantity = s.quantity;
+                        }
+                    }
+                    plus = !plus;
+                }
+                if (reduction < maxReduction) {
+                    move = path;
+                    leaving = leavingCandidate;
+                    maxReduction = reduction;
+                }
+            }
+        }
+
+        if (move != null) {
+            double q = leaving.quantity;
+            boolean plus = true;
+            for (Shipment s : move) {
+                s.quantity += plus ? q : -q;
+                matrix[s.r][s.c] = s.quantity == 0 ? null : s;
+                plus = !plus;
+            }
+            steppingStone();
+        }
+    }
+
+    static LinkedList<Shipment> matrixToList() {
+        return stream(matrix).flatMap(row -> stream(row)).filter(s -> s != null).collect(toCollection(LinkedList::new));
+    }
+
+    static Shipment[] getClosedPath(Shipment s) {
+        LinkedList<Shipment> path = matrixToList();
+        path.addFirst(s);
+
+        // remueve (y sigue removiendo) elementos que no tienen un
+        // vecino Horizontal Y vertical
+        while (path.removeIf(e -> {
+            Shipment[] nbrs = getNeighbors(e, path);
+            return nbrs[0] == null || nbrs[1] == null;
+        }))
+            ;
+
+        // coloca los elementos restantes en un orden de mas-menos
+        Shipment[] stones = path.toArray(new Shipment[path.size()]);
+        Shipment prev = s;
+        for (int i = 0; i < stones.length; i++) {
+            stones[i] = prev;
+            prev = getNeighbors(prev, path)[i % 2];
+        }
+        return stones;
+    }
+
+    static Shipment[] getNeighbors(Shipment s, LinkedList<Shipment> lst) {
+        Shipment[] nbrs = new Shipment[2];
+        for (Shipment o : lst) {
+            if (o != s) {
+                if (o.r == s.r && nbrs[0] == null)
+                    nbrs[0] = o;
+                else if (o.c == s.c && nbrs[1] == null)
+                    nbrs[1] = o;
+                if (nbrs[0] != null && nbrs[1] != null)
+                    break;
+            }
+        }
+        return nbrs;
+    }
+
+    static void fixDegenerateCase() {
+        final double eps = Double.MIN_VALUE;
+        // Verifica que se cumpla la LEY DE LA NO DEGENERACION
+        if (supply.length + demand.length - 1 != matrixToList().size()) {
+
+            for (int r = 0; r < supply.length; r++)
+                for (int c = 0; c < demand.length; c++) {
+                    if (matrix[r][c] == null) {
+                        Shipment dummy = new Shipment(eps, costs[r][c], r, c);
+                        if (getClosedPath(dummy).length == 0) {
+                            matrix[r][c] = dummy;
+                            return;
+                        }
+                    }
+                }
+        }
+    }
+
+    static void printDetailedResult() {
+        System.out.printf("SOLUCION OPTIMA: %n%n");
+        double totalCosts = 0;
+        for (int r = 0; r < supply.length; r++) {
+            for (int c = 0; c < demand.length; c++) {
+
+                Shipment s = matrix[r][c];
+                if (s != null && s.r == r && s.c == c) {
+                    System.out.printf(" %3s ", (int) s.quantity);
+                    totalCosts += (s.quantity * s.costPerUnit);
+                } else
+                    System.out.printf("  -  ");
+            }
+            System.out.println();
+        }
+
+        for (int r = 0; r < supply.length; r++) {
+            for (int c = 0; c < demand.length; c++) {
+
+                Shipment s = matrix[r][c];
+                if (s != null && s.r == r && s.c == c) {
+                    System.out.printf("Se debe enviar "+s.quantity+" unidades desde P"+(r+1)+" hasta A"+(c+1)+"\n");   
+                }
+            }
+        }
+        System.out.printf("%n Para tener un COSTO MINIMO de: %s%n%n", totalCosts);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        String filename = "C:/Users/Nicolas Carreno/Documents/Universidad/Optimizacion/input1.txt";
+        init(filename);
+        northWestCornerRule();
+        steppingStone();
+        printDetailedResult();
+    }
+}
